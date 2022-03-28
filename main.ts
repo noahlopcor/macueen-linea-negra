13cd4a416377b77d364486af63559255d733ccc0
noahlopcor <99145902+noahlopcor@users.noreply.github.com>
2022-03-28 11:27:44 +0200

    ññññññññññññññññññ

--- main.ts
+++ main.ts
@@ -1,8 +1,13 @@
 basic.forever(function () {
     if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 20)
+        maqueen.motorStop(maqueen.Motors.All)
         basic.pause(100)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 15)
+        basic.pause(100)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 15)
+        basic.pause(500)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 15)
     } else {
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 20)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 15)
     }
 })
